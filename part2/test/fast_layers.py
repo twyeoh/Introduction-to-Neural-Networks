fc0c5c0d802874f97b6ec1b25699be1cc903fac0
twyeoh <44494062+twyeoh@users.noreply.github.com>
2018-11-27 11:41:11 +0800

    Update fast_layers.py

--- part2/test/fast_layers.py
+++ part2/test/fast_layers.py
@@ -1,13 +1,13 @@
 import numpy as np
 try:
-  from cs294_129.im2col_cython import col2im_cython, im2col_cython
-  from cs294_129.im2col_cython import col2im_6d_cython
+  from test.im2col_cython import col2im_cython, im2col_cython
+  from test.im2col_cython import col2im_6d_cython
 except ImportError:
   print 'run the following from the cs294_129 directory and try again:'
   print 'python setup.py build_ext --inplace'
   print 'You may also need to restart your iPython kernel'
 
-from cs294_129.im2col import *
+from test.im2col import *
 
 
 def conv_forward_im2col(x, w, b, conv_param):
